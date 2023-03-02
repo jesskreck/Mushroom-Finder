111a3ef46b86e9ba10ab96fb041c0aa2a42b9ff4
jesskreck <krecker.jessica@gmail.com>
2023-03-02 11:16:35 +0100

    createTags() now working

--- FAQ-content.js
+++ FAQ-content.js
@@ -1,4 +1,4 @@
-const pilzradar = [
+const pilzradarrr = [
   {
     "0": 1005,
     "1": "22.02.2023",
@@ -518,5 +518,48 @@ const pilzradar = [
     "Sammler": "Tamara",
     "Bemerkungen": "Keine",
     "datum": "2023-01-11 16:46:36"
+  }
+]
+
+
+const Pilzwissen = [
+  {
+    "PILZ": {
+      "Allgemein": "TEXT",
+      "Vorkommen": "TEXT",
+      "Makroskopische Merkmale": "TEXT",
+      "Speisewert": "TEXT",
+      "Verwechslungsgefahr": "TEXT",
+    },
+    "Gemeiner Samtfußrübling": {
+      "Allgemein": "In Japan ist der Gemeine Samtfußrübling (Flammulina velutipes) ein echter Star unter den Pilzen. Dort ist er als Enokitake oder kurz Enoki bekannt und nach dem Shiitake der meist konsumierte Speisepilz. Jährlich werden etwa 100.000 Tonnen Enoki produziert. Da die frostresistenten Fruchtkörper in Deutschland im kühlen Winterhalbjahr wachsen, wird er hier auch Winterpilz genannt.",
+      "Vorkommen": "Der Gemeine Samtfußrübling (Flammulina velutipes) ist ein Saprophyt und Schwächeparasit, der seine Fruchtkörper meist büschelig in der Zeit von Oktober bis März ausbildet. Zu finden ist er an Stämmen, Stümpfen und Ästen von Laubholz. Er bevorzugt Weide, Pappel, Esche und Holunder.",
+      "Makroskopische Merkmale": "Die Fruchtkörper des Gemeinen Samtfußrüblings erscheinen im typischen Rüblingshabitus und erreichen eine Größe von 2 bis 10 cm. Sie sind auffällig honiggelb bis rotorange, dünnfleischig und schmierig. Die Lamellen sind cremfarben bis blassgelb. Das Sporenpulver ist weiß. Das markanteste Merkmal ist der faserige, zähe, im Alter hohlwerdende Stiel. Er ist von der Basis mehr oder weniger braunsamtig überzogen und geht über in eine gelbliche Stielspitze.",
+      "Speisewert": "Der Gemeine Samtfußrübling ist ein hervorragender Speisepilz, der vorallem in Japan auch kultiviert wird. In der Küche sollten nur die Hüte verwendet werden, da die Stiele sehr zäh sind. Er zählt auch zu den Heilpilzen und wird zur Behandlung von Lebererkrankungen, von Bluthochdruck, bei Magenschleimhautentzündungen und zur Vorbeugung von Thrombosen eingesetzt. Er wirkt zudem antiviral und immunstimulierend und soll sogar eine Antitumorwirkung haben.",
+      "Verwechslungsgefahr": "Von unkundigen Sammlern kann der Gemeine Samtfußrübling mit dem essbaren Stockschwämmchen (Kuehneromyces mutabulis) oder dem tödlich giftigen Gift-Häubling (Galerina marginata) verwechselt werden. Durch den braunsamtigen und ringlosen Stiel ist der Samtfußrübling jedoch sicher zu erkennen.",
+    },
+    "Striegelige Tramete": {
+      "Allgemein": "Die Striegelige Tramete (Trametes hirsuta) ist ein typischer Vertreter der Gattung der Trameten (Trametes). Aufgrund der sich angenehm weich anfühlenden Behaarung junger Fruchtkörper wird der Pilz manchmal auch als „Sofa-Pilz“ bezeichnet.",
+      "Vorkommen": "Als sehr häufiger und sehr weit verbreiteter Saprobiont besiedelt die Striegelige Tramete Stümpfe, Stämme und Äste von Laubhölzern. Sie bevorzugt die Rotbuche, ist aber auch an Eichen, Birken, Eschen und selten an anderen Bäumen zu finden. Die einjährigen Fruchtkörper der Striegeligen Trameten sind ganzjährig zu finden.",
+      "Makroskopische Merkmale": "Die meist konsolenförmigen, dachziegelartig übereinander stehenden, dünnfleischigen Fruchtkörper der Striegeligen Tramete erreichen einen Durchmesser von 5 bis 8, selten bis zu 15 cm. Die wellig gezonte Oberseite ist weiß, grau oder cremefarben und striegelig behaart. Bei jungen Fruchtkörpern ist die Behaarung weichlich und erinnert an ein Plüschsofa. Die Zuwachszone ist etwas dunkler bräunlich gefärbt. Die Röhrenschicht der Unterseite ist weißlich bis cremefarben, manchmal ist auch ein Grauschimmer zu sehen. Die Poren sind rundlich und recht grob (ca. 0,5 mm im Durchmesser). Das Sporenpulver ist weiß. Das weißliche bis graue zähe Fruchtfleisch ist zweischichtig aufgebaut und die beiden Schichten sind durch eine schwarze Linie voneinander getrennt. Geruch und Geschmack sind neutral.",
+      "Speisewert": "kein Speisepilz",
+      "Verwechslungsgefahr": "Die Striegelige Tramete kann mit der Buckeligen Tramete (Trametes gibbosa) oder mit der Samtigen Tramete (Trametes pubescens) verwechselt werden. Die buckelige Tramete ist meist von größerem Wuchs und ist buckelartig verdickt am Substrat angewachsen. Die Samtige Tramete ist im Alter gilbend und zeigt deutlich feinere Poren (3-4 je mm). In Deutschland ist die samtige Tramete selten und lediglich in den Alpen und Voralpen regelmäßig zu finden.",
+    },
+    "Gifthäubling": {
+      "Allgemein": "Der Nadelholz-Häubling (Galerina marginata) ist tödlich giftig und wird daher völlig zu Recht auch als Gift-Häubling bezeichnet. Er enthält wie der Grüne Knollenblätterpilz (Amanita phalloides) Amatoxine und gilt als gefährlicher „Doppelgänger“ des wohlschmeckenden Stockschwämmchens (Kuehneromyces mutabilis).",
+      "Vorkommen": "Der Saprobiont besiedelt meist morsche Stümpfe, Stämme und Äste von Nadelhölzern. Meist ist er an abgestorbenen Fichten und Kiefern und nur selten an Laubhölzern zu finden. Er fruktifiziert in der Regel in den Monaten von September bis November.",
+      "Makroskopische Merkmale": "Die halbkugeligen oder glockigen bis flachkonvex ausgebreiteten Hüte erreichen einen Durchmesser von 1 bis 5 cm. Sie sind hygrophan. Im durchfeuchteten Zustand sind sie ockerbräunlich bis rotbraun. Beim Trocknen verblassen sie hellocker. Die Huthaut ist kahl und trocken. Der Hutrand ist etwas überstehend und scharfkantig und manchmal mit flüchtigen Velumresten behangen. Die Lamellen sind jung gelblich und verfärben sich durch das braune bis rostbraune Sporenpulver bald rostbraun. Sie sind schwach ausgebuchtet bis breit am Stiel angewachsen und oft mit einem Zahn an diesem herablaufend. Der Stiel ist jung wie der Hut gefärbt, wird im Alter meist dunkler und trägt einen häutigen vergänglichen Ring. Unterhalb des Rings ist der Stiel weißlich bis silbrig überfasert. Das Fruchtfleisch ist im Hut hell gelblich und im Stiel umberbraun. Geruch und Geschmack sind meist mehlig, manchmal auch rettichartig oder fehlend. Aufgrund der strengen Giftigkeit sollte von Geschmacksproben abgesehen werden.",
+      "Speisewert": "Der Nadelholz-Häubling ist streng giftig und verursacht das Phalloides-Syndrom. Der Pilz enthält die gleichen Amatoxine wie der Grüne Knollenblätterpilz. Eine nicht rechtzeitig erkannte Vergiftung kann tödlich enden.",
+      "Verwechslungsgefahr": "Der Nadelholz-Häubling gilt als der tödlich giftige „Doppelgänger“ des wohlschmeckenden Stockschwämmchens (Kuehneromyces mutabilis), das im Unterschied zum Nadelholz-Häubling bevorzugt (aber nicht ausschließlich) an Laubhölzern wächst. Das Stockschwämmchen riecht angenehm pilzig und nicht mehlig oder rettichartig. Das sicherste Unterscheidungsmerkmal ist jedoch der beim Stockschwämmchen unterhalb der Ringzone mit bräunlichen Schüppchen besetzte Stiel.",
+    },
+    "Judasohr": {
+      "Allgemein": "Aktuell findet man unseren Pilz der Woche nahezu überall in den Wäldern. Judasohren (Auricularia auricula-judae)  bieten dem fleißigen Sammler auch in den frostigen Monaten eine Möglichkeit, sein Körbchen mit Speisepilzen zu füllen. Das Judasohr wird auch als Holunderschwamm, Ohrlappenpilz oder Wolkenohrenpilz bezeichnet.",
+      "Vorkommen": "Das Judasohr (Auricularia auricula-judae) ist ein Schwächeparasit und Saprophyt, der die Weißfäule auslöst. Es ist ganzjährig zu finden und da die Fruchtkörper frostresistent sind, kann man sie auch im tiefgefrorenen Zustand sammeln. Er wächst an zahlreichen Laubholzbäumen, wie zum Beispiel Birken, Ulmen, Ahorn und nur sehr selten an Nadelholzbäumen. Eindeutig präferiert das judasohr jedoch Holunder!",
+      "Makroskopische Merkmale": "Das Judasohr gehört zu den Gallertpilzen. Seine 3 bis 10 cm breiten Fruchtkörper sind ohrmuschelförmig und von einer gelatinösen knorpeligen mitunder auch zähen Konsistenz. Die Oberfläche ist fleisch- bis rotbräunlich und sehr feinfilzig. Die Unterseite ist stets heller. Die Fruchtkörper sind mehr oder weniger intensiv von „Adern“ durchzogen und schmal am Substrat angewachsen. Das Sporenpulver ist weiß.",
+      "Speisewert": "Die Pilze werden vor allem in der asiatischen Küche sehr geschätzt. Unter dem Namen „China Morchel“ findet man sie als Ergänzung zum Gemüse in vielen Gerichten. Sie eignen sich auch sehr gut für Suppen. Auch in der traditionellen chinesischen Medizin finden sie Verwendung. Man sagt ihnen nach, eine entzündungshemmende Wirkung zu haben, ebenso sollen sie bei Kreislaufproblemen helfen.",
+      "Verwechslungsgefahr": "Eine geringe Verwechslungsgefahr besteht mit dem Gezonten Ohrlappenpilz (Auricularia mesenterica), der eine gezonte und struppig-filzige Oberfläche und eine dunklere Unterseite aufweißt.",
+    },
+
+
   }
 ]
--- main.js
+++ main.js
@@ -1,72 +1,165 @@
 function fetchMushrooms() {
-    fetch("https://www.pilzradar.de/api/prRestAPIv3-JessicaKrecker-pilzradar.php")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (result) {
-            console.log(result);
-            createGallery(result);
-            createCards(result);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+  fetch("https://www.pilzradar.de/api/prRestAPIv3-JessicaKrecker-pilzradar.php")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      console.log(result);
+      createCards(result);
+      createTags(result);
+
+      // ab hier testing
+      checkSpeisepilz(result);
+      activateFilters;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
 fetchMushrooms();
 
+//////////////////////////////////////////////// hamburger menu  ////////////////////////////////////////////////
 
+const menuToggle = document.querySelector(".menu-toggle");
+const sidebar = document.querySelector(".sidebar");
 
-//////////////////////////////////////////////////// creating gallery with images  ////////////////////////////////////////////////////
+menuToggle.addEventListener("click", function () {
+  menuToggle.classList.toggle("is-active");
+  sidebar.classList.toggle("is-active");
+});
 
+//////////////////////////////////////////////// creating gallery with images  ////////////////////////////////////////////////
 
-function createGallery(array) {
-    const gallery = document.getElementById("gallery");
-    for (let i = 0; i < array.length; i++) {
-        const img = document.createElement("img");
-        let imgURL = "https://www.pilzradar.de/" + array[i][5];
-        img.setAttribute("src", imgURL);
-        gallery.append(img);
-    }
-}
+// function createGallery(array) {
+//   const gallery = document.getElementById("gallery");
+//   for (let i = 0; i < array.length; i++) {
+//     const img = document.createElement("img");
+//     let imgURL = "https://www.pilzradar.de/" + array[i][5];
+//     img.setAttribute("src", imgURL);
+//     gallery.append(img);
+//   }
+// }
 
-
-//////////////////////////////////////////////////// creating cards ////////////////////////////////////////////////////
+//////////////////////////////////////////////// creating cards ////////////////////////////////////////////////
 
 function createCards(array) {
-    for (let i = 0; i < array.length; i++) {
-        const MushroomCardGallery = document.getElementById("mushroom-card-gallery");
-        const card = document.createElement("article");
-        MushroomCardGallery.append(card);
-        card.classList.add("card-B");
+  for (let i = 0; i < array.length; i++) {
+    const MushroomCardGallery = document.getElementById(
+      "mushroom-card-gallery"
+    );
+    const card = document.createElement("article");
+    MushroomCardGallery.append(card);
+    card.classList.add("card-B");
 
-        // header
-        const header = document.createElement("div");
-        card.append(header);
-        header.classList.add("card-B__HEADER");
-        
-        // background image
-        const a = document.createElement("a");
-        card.append(a);
-        const cardImage = document.createElement("div");
-        a.append(cardImage);
-        cardImage.classList.add("card-B__img");
-        let imgURL = "https://www.pilzradar.de/" + array[i][5];
-        console.log(imgURL);
-        cardImage.style.cssText += `background-image:url(${imgURL})`;     // .style.backgroundImage = URL war nicht möglich, weil Variable dort nicht eingetragen werden konnte
-        
-        // footer
-        const footer = document.createElement("div");
-        card.append(footer);
-        footer.classList.add("card-B__FOOTER");
-        const name = document.createElement("h2");
-        footer.append(name);
-        name.innerText = array[i][2];
-        const description = document.createElement("div");
-        footer.append(description);
-        description.innerText = "von " + array[i][9] + " in " + array[i][6] + " (" + array[i][4] + ")";
-    }
+    // create header
+    const header = document.createElement("div");
+    card.append(header);
+    header.classList.add("card-B__HEADER");
+
+    // set database image as background image
+    const a = document.createElement("a");
+    card.append(a);
+    const cardImage = document.createElement("div");
+    a.append(cardImage);
+    cardImage.classList.add("card-B__img");
+    let imgURL = "https://www.pilzradar.de/" + array[i][5];
+    cardImage.style.cssText += `background-image:url(${imgURL})`;
+    
+    // add link to details page to image 
+    a.setAttribute("href", "details.html");
+
+    // fill footer with details
+    const footer = document.createElement("div");
+    card.append(footer);
+    footer.classList.add("card-B__FOOTER");
+    const name = document.createElement("h2");
+    footer.append(name);
+    name.innerText = array[i][2];
+    const description = document.createElement("div");
+    footer.append(description);
+    description.innerText =
+      "von " + array[i][9] + " in " + array[i][6] + " (" + array[i][4] + ")";
+    
+    // add #id
+    card.id = array[i][0];
+  }
 }
+
+
+
+//////////////////////////////////////////////// creating tags ////////////////////////////////////////////////
+
+function createTags(array)
+{
+  const verwendung = document.getElementById("verwendung");
+  let set = new Set();
+  for (let i = 0; i < array.length; i++)
+  {
+    set = set.add(array[i].Verwendung);
+  };
+  for (let item of set)
+  {
+    let tag = document.createElement("div");
+    tag.classList.add("btn-tag");
+    verwendung.append(tag);
+    tag.innerText = item;
+    tag.id = item;
+  } 
+}
+
+
+
+// function makeTagAsButton(array) {
+//   const buttons = document.querySelectorAll(".btn-tag");
+//   console.log(buttons);
+//   for (i = 0; i < buttons.length; i++)
+//   {
+//     buttons[i].id = "btnFilter" + i;
+//   };
+// }
+
+
+
+//////////////////////////////////////////////// filter  ////////////////////////////////////////////////
+
+
+
+SpeisepilzFilter.addEventListener("click", showOnlySpeisepilze);
+
+function checkSpeisepilz(array) {
+  if (array.Verwendung == "Speisepilz")
+    return true;
+  else
+    return false;  
+}
+
+function showOnlySpeisepilze(array) {
+  let Speisepilze = array.filter(checkSpeisepilz);
+  console.log("erfolg");
+  createCards(Speisepilze);
+}
+
+
+const SpeisepilzFilter = document.getElementById("Speisepilz");
+
+
+
+
+
+function buildDetailsPage(array) {
+  
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -76,16 +169,15 @@ function createCards(array) {
 
 
 //////////////// Get location and pilzfreund
-const cards = document.getElementById("cards")
+// const cards = document.getElementById("cards");
 
-for (let i = 0; i < pilzradar.length; i++) {
-    const mushroom = document.createElement("div");
-    cards.append(mushroom);
-    const name = document.createElement("h2");
-    mushroom.append(name);
-    name.innerText = pilzradar[i][2];
-    const location = document.createElement("p");
-    mushroom.append(location);
-    location.innerText = pilzradar[i].Landkreis + ", " + pilzradar[i].Bundesland;
-    
-}
+// for (let i = 0; i < pilzradar.length; i++) {
+//   const mushroom = document.createElement("div");
+//   cards.append(mushroom);
+//   const name = document.createElement("h2");
+//   mushroom.append(name);
+//   name.innerText = pilzradar[i][2];
+//   const location = document.createElement("p");
+//   mushroom.append(location);
+//   location.innerText = pilzradar[i].Landkreis + ", " + pilzradar[i].Bundesland;
+// }
